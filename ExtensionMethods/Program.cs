87541b9a1f5175c41968421fe18248debfbbccd0
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2022-03-03 13:42:42 -0700

    Extension Methods

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,38 @@
+﻿/// Notes
+/// Extension methods allow you to add functionality from existing classes
+/// 
+using System;
+
+namespace ExtentionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is actually a very long message.  Like its a blog post or something.  blah blah blah...";
+
+            var shortenedPost = post.Shorten(2);
+            Console.WriteLine(shortenedPost);
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException(nameof(numberOfWords), "must be a positive number");
+
+            if (numberOfWords == 0)
+                return "";
+
+            var words = str.Split(' ');
+            
+            if(words.Length <= numberOfWords)
+                return str;
+            
+            return string.Join(' ', words[0..numberOfWords]);
+        }
+    }
+}
+
