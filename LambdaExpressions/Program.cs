9eb17a55bf8a9a1af57c0110deceaa23490d103e
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2022-03-03 11:54:35 -0700

    Lambda Expressions

---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,25 @@
+﻿// Notes
+// A lambda is an anonymous method - it has no access modifier, name, or return statement.
+// We use them for convenience.
+
+// Syntax: args => expression
+
+// to use a lambda, we need to assign it to a delegate
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // <arg, return value>
+            Func<int, int> square = number => number * number;
+            Console.WriteLine(square(55));
+        }
+
+        //static int Square(int number)
+        //{
+        //    return number * number;
+        //}
+    }
+}
